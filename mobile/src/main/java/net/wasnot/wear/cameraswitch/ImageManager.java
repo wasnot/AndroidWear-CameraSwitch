ea6ef154714124ece770ebcdd9cac3d28e09f527
wasnot <a.aida@freebit.net>
2014-07-09 10:11:17 +0900

    first commit

---
+++ mobile/src/main/java/net/wasnot/wear/cameraswitch/ImageManager.java
@@ -0,0 +1,66 @@
+package net.wasnot.wear.cameraswitch;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by aidaakihiro on 2014/07/09.
+ */
+public class ImageManager {
+
+    public static Uri addImageAsCamera(ContentResolver cr, Bitmap bitmap) {
+        long dateTaken = System.currentTimeMillis();
+        String name = createName(dateTaken) + ".jpg";
+        String uriStr = MediaStore.Images.Media.insertImage(cr, bitmap, name,
+                name);
+        return Uri.parse(uriStr);
+    }
+
+    private static String createName(long dateTaken) {
+        return DateFormat.format("yyyy-MM-dd_kk.mm.ss", dateTaken).toString();
+    }
+
+    public static Bitmap getBitmapImageFromYUV(byte[] data, int width, int height) {
+        YuvImage yuvimage = new YuvImage(data, ImageFormat.NV21, width, height, null);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        yuvimage.compressToJpeg(new Rect(0, 0, width, height), 80, baos);
+        byte[] jdata = baos.toByteArray();
+        BitmapFactory.Options bitmapFatoryOptions = new BitmapFactory.Options();
+        bitmapFatoryOptions.inPreferredConfig = Bitmap.Config.RGB_565;
+        Bitmap bmp = BitmapFactory.decodeByteArray(jdata, 0, jdata.length, bitmapFatoryOptions);
+        return bmp;
+    }
+
+    public static final void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height) {
+        final int frameSize = width * height;
+        for (int j = 0, yp = 0; j < height; j++) {
+            int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
+            for (int i = 0; i < width; i++, yp++) {
+                int y = (0xff & ((int) yuv420sp[yp])) - 16;
+                if (y < 0) y = 0;
+                if ((i & 1) == 0) {
+                    v = (0xff & yuv420sp[uvp++]) - 128;
+                    u = (0xff & yuv420sp[uvp++]) - 128;
+                }
+                int y1192 = 1192 * y;
+                int r = (y1192 + 1634 * v);
+                int g = (y1192 - 833 * v - 400 * u);
+                int b = (y1192 + 2066 * u);
+                if (r < 0) r = 0; else if (r > 262143) r = 262143;
+                if (g < 0) g = 0; else if (g > 262143) g = 262143;
+                if (b < 0) b = 0; else if (b > 262143) b = 262143;
+                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
+            }
+        }
+    }
+
+}
---
+++ mobile/src/main/java/net/wasnot/wear/cameraswitch/MainActivity.java
@@ -0,0 +1,254 @@
+package net.wasnot.wear.cameraswitch;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder holder;
+    private int numberOfCameras;
+    private int defaultCameraId;
+    private int cameraCurrentlyLocked;
+
+    private SurfaceHolder.Callback mSurfaceListener =
+            new SurfaceHolder.Callback() {
+                public void surfaceCreated(SurfaceHolder holder) {
+                    mCamera = Camera.open();
+                    try {
+                        mCamera.setDisplayOrientation(90);
+                        Camera.Parameters parameters = mCamera.getParameters();
+                        List<Camera.Size> list = parameters.getSupportedPreviewSizes();
+                        Toast.makeText(MainActivity.this, "test:" + list.size(), Toast.LENGTH_LONG)
+                                .show();
+                        for (Camera.Size size : list) {
+                            Log.d("camera", "size:" + size.width + "," + size.height);
+                        }
+                        holder.setFixedSize(list.get(0).width, list.get(0).height);
+                        mCamera.setPreviewDisplay(holder);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    if (mCamera == null) {
+                        return;
+                    }
+                    mCamera.release();
+                    mCamera = null;
+                }
+
+                public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                        int height) {
+                    if (mCamera == null) {
+                        return;
+                    }
+//                    Camera.Parameters parameters = mCamera.getParameters();
+//                    parameters.setPreviewSize(width, height);
+//                    mCamera.setParameters(parameters);
+                    setPreviewSize(width, height);
+                    mCamera.startPreview();
+                }
+            };
+    // シャッターが押されたときに呼ばれるコールバック
+    private Camera.ShutterCallback mShutterListener =
+            new Camera.ShutterCallback() {
+                public void onShutter() {
+//                    Toast.makeText(MainActivity.this, "tdst", Toast.LENGTH_SHORT).show();
+                }
+            };
+
+    // JPEGイメージ生成後に呼ばれるコールバック
+    private Camera.PictureCallback mPictureListener =
+            new Camera.PictureCallback() {
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    // SDカードにJPEGデータを保存する
+                    if (data != null) {
+//                        FileOutputStream myFOS = null;
+//                        try {
+//                            myFOS = new FileOutputStream(
+//                                    "/sdcard/camera_test" + System.currentTimeMillis() + ".jpg");
+//                            myFOS.write(data);
+//                            myFOS.close();
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+                        Bitmap bmp = null;
+                        bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+//                        // 読み込む範囲
+//                        int previewWidth = camera.getParameters().getPreviewSize().width;
+//                        int previewHeight = camera.getParameters().getPreviewSize().height;
+//
+//                        // プレビューデータから Bitmap を生成
+//                        Bitmap bmp = ImageManager.getBitmapImageFromYUV(data, previewWidth,
+//                                previewHeight);
+                        ImageManager.addImageAsCamera(getContentResolver(), bmp);
+
+                        camera.startPreview();
+                    }
+                }
+            };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//            if (mCamera != null) {
+//                mCamera.takePicture(mShutterListener, null, mPictureListener);
+//            }
+//        }
+//        return true;
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        holder = mSurfaceView.getHolder();
+        holder.addCallback(mSurfaceListener);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCamera != null) {
+                    mCamera.takePicture(mShutterListener, null, mPictureListener);
+                }
+            }
+        });
+
+        // 利用可能なカメラの個数を取得
+        numberOfCameras = Camera.getNumberOfCameras();
+
+        // CameraInfoからバックフロントカメラのidを取得
+        CameraInfo cameraInfo = new CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                defaultCameraId = i;
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        menu.add(Menu.NONE,1,0,"camera");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            // カメラが複数あるかチェック
+            if (numberOfCameras == 1) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("no more camera")
+                        .setNeutralButton("Close", null);
+                AlertDialog alert = builder.create();
+                alert.show();
+                return true;
+            }
+
+            // 現在利用しているカメラを解放
+            if (mCamera != null) {
+                mCamera.stopPreview();
+//                mPreview.setCamera(null);
+                mCamera.release();
+                mCamera = null;
+            }
+
+            // カメラを切り替え
+            mCamera = Camera.open(
+                    (cameraCurrentlyLocked + 1) % numberOfCameras);
+            cameraCurrentlyLocked = (cameraCurrentlyLocked + 1)
+                    % numberOfCameras;
+//            mPreview.switchCamera(mCamera);
+            try {
+                mCamera.setDisplayOrientation(90);
+                mCamera.setPreviewDisplay(holder);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // プレビュー再開
+            mCamera.startPreview();
+            return true;
+        }else if(item.getItemId()==1){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 0);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setPreviewSize(int width, int height) {
+        Camera.Parameters mParam = mCamera.getParameters();
+        boolean portrait = true;
+        // Set width & height
+        int previewWidth = width;
+        int previewHeight = height;
+        if (portrait) {
+            previewWidth = height;
+            previewHeight = width;
+        }
+
+        List<Camera.Size> sizes = mParam.getSupportedPreviewSizes();
+        int tmpHeight = 0;
+        int tmpWidth = 0;
+        for (Camera.Size size : sizes) {
+            if ((size.width > previewWidth) || (size.height > previewHeight)) {
+                continue;
+            }
+            if (tmpHeight < size.height) {
+                tmpWidth = size.width;
+                tmpHeight = size.height;
+            }
+        }
+        previewWidth = tmpWidth;
+        previewHeight = tmpHeight;
+
+        mParam.setPreviewSize(previewWidth, previewHeight);
+
+        // Adjust SurfaceView size
+        ViewGroup.LayoutParams layoutParams = mSurfaceView.getLayoutParams();
+        if (portrait) {
+            layoutParams.height = previewWidth;
+            layoutParams.width = previewHeight;
+        } else {
+            layoutParams.height = previewHeight;
+            layoutParams.width = previewWidth;
+        }
+        mSurfaceView.setLayoutParams(layoutParams);
+
+//        mCamera.setParameters(mParam);
+    }
+}
